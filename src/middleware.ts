cc6a586e76b754d8a63fbbf9c4248be0e8a7b922
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-06-20 11:28:18 +0530

    refactor: protect studio route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/feed/subscriptions', '/playlists(.*)']);
+const isProtectedRoute = createRouteMatcher(['/studio(.*)', '/feed/subscriptions(.*)', '/playlists(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
 	if (isProtectedRoute(req)) await auth.protect();
