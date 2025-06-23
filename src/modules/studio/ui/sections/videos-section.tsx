cdaaaa9011fcd6a56c0cb44f0059a189187c4e97
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-06-23 17:25:16 +0530

    feat: add videos section table skeleton loading state

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -10,6 +10,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { VideoThumbnail } from '@/modules/videos/ui/components/video-thumbnail';
 
 import { InfiniteScroll } from '@/components/infinite-scroll';
+import { Skeleton } from '@/components/ui/skeleton';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { DEFAULT_LIMIT } from '@/constants';
 import { MuxStatus, VideoVisibility } from '@/db/schema';
@@ -18,11 +19,68 @@ import { trpc } from '@/trpc/client';
 
 export const VideosSection = () => {
 	return (
-		<Suspense fallback={<p>Loading...</p>}>
+		<Suspense fallback={<VideosSectionSkeleton />}>
 			<ErrorBoundary fallback={<p>Error...</p>}>
 				<VideosSectionSuspense />
 			</ErrorBoundary>
 		</Suspense>
+	);
+};
+
+const VideosSectionSkeleton = () => {
+	return (
+		<>
+			<div className='border-y'>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className='w-[510px] pl-6'>Video</TableHead>
+							<TableHead>Visibility</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead>Date</TableHead>
+							<TableHead className='text-right'>Views</TableHead>
+							<TableHead className='text-right'>Comments</TableHead>
+							<TableHead className='pr-6 text-right'>Likes</TableHead>
+						</TableRow>
+					</TableHeader>
+
+					<TableBody>
+						{Array.from({ length: 5 }).map((_, index) => (
+							<TableRow key={index}>
+								<TableCell className='w-[510px] pl-6'>
+									<div className='flex items-center gap-4'>
+										<Skeleton className='h-20 w-36' />
+										<div className='flex flex-col gap-2'>
+											<Skeleton className='h-4 w-[100px]' />
+											<Skeleton className='h-3 w-[150px]' />
+										</div>
+									</div>
+								</TableCell>
+
+								<TableCell>
+									<Skeleton className='h-4 w-20' />
+								</TableCell>
+								<TableCell>
+									<Skeleton className='h-4 w-16' />
+								</TableCell>
+								<TableCell>
+									<Skeleton className='h-4 w-24' />
+								</TableCell>
+								<TableCell className='text-right'>
+									<Skeleton className='ml-auto h-4 w-12' />
+								</TableCell>
+								<TableCell className='text-right'>
+									<Skeleton className='ml-auto h-4 w-12' />
+								</TableCell>
+								<TableCell className='pr-6 text-right'>
+									<Skeleton className='ml-auto h-4 w-12' />
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</div>
+		</>
 	);
 };
 
