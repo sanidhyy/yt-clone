a9c01ff482ad9ec631cd3bae77240445f644ca48
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-07-30 16:49:27 +0530

    feat: comment loading state skeleton

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -2,6 +2,7 @@
 
 import { Suspense } from 'react';
 
+import { Loader2Icon } from 'lucide-react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { CommentForm } from '@/modules/comments/ui/components/comment-form';
@@ -17,11 +18,19 @@ interface CommentsSectionProps {
 
 export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
 	return (
-		<Suspense fallback={<p>Loading...</p>}>
+		<Suspense fallback={<CommentsSectionSkeleton />}>
 			<ErrorBoundary fallback={<p>Error...</p>}>
 				<CommentsSectionSuspense videoId={videoId} />
 			</ErrorBoundary>
 		</Suspense>
+	);
+};
+
+const CommentsSectionSkeleton = () => {
+	return (
+		<div className='mt-6 flex items-center justify-center'>
+			<Loader2Icon className='size-7 animate-spin text-muted-foreground' />
+		</div>
 	);
 };
 
